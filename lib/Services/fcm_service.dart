1ff8c3de7b48e3b3870deb204e562836f3aec5f2
Alyvvn <muhammadalyan27@gmail.com>
2025-12-10 22:39:19 -0500

    Add FCM service

---
+++ lib/Services/fcm_service.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class FcmService {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> registerDevice(String userId) async {
+    // Request permission to receive notifications
+    final settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      sound: true,
+    );
+
+    // If the user has not granted permission to receive notifications, return
+    if (settings.authorizationStatus != AuthorizationStatus.authorized) {
+        debugPrint('User has not granted permission to receive notifications');
+        return;
+    }
+
+    // Get the token for the device
+    final token = await _firebaseMessaging.getToken();
+
+    if (token == null) {
+        debugPrint('Failed to get FCM token');
+        return;
+    }
+    if(token != null){
+        
+        await _firestore.collection('users').doc(userId).update({
+    'fcmToken': token,
+    "fcmTokenLastUpdated": DateTime.now(),
+    }, SetOptions(merge: true));
+    }
+  }
+  _FirebaseMessaging.ontokenrefreshed.listen((event) async {
+    await _firestore.collection('users').doc(userId).update({
+      'fcmToken': event.token,
+      "fcmTokenLastUpdated": DateTime.now(),
+    }, SetOptions(merge: true));
+  });
+
+  Future<void> initialize() async {
+    await _firebaseMessaging.requestPermission();
+  }
+}
