4819a036c5d743657747a84b2324d0765405afdd
Nathan Larsen <nlarsen@nathan2055.com>
2025-12-12 20:30:38 -0500

    Delete user database functions from FirestoreHelper as these will now be managed by Firebase Auth via the AuthService class

--- lib/models/firestore_helper.dart
+++ lib/models/firestore_helper.dart
@@ -1,82 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:mentalzen/models/user_entry.dart';
 import 'package:mentalzen/models/chat_entry.dart';
 
 // FirestoreHelper handles all interaction with the Cloud Firestore databases
 class FirestoreHelper {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // Adds a new user entry
-  void addUserEntry(UserEntry user) {
-    final docRef = _firestore
-        .collection('users')
-        .withConverter(
-          fromFirestore: UserEntry.fromFirestore,
-          toFirestore: (UserEntry userEntry, options) =>
-              userEntry.toFirestore(),
-        )
-        .doc(user.email);
-    docRef.set(user);
-  }
-
-  // Updates an existing user entry's profile details by getting
-  // the existing UserEntry from Cloud Firestore, converting it to a map,
-  // applying the profile updates, converting it back to a map,
-  // and then saving the result back to Cloud Firestore
-  Future<bool> updateUserProfile(
-    String emailID,
-    String newUsername,
-    String newFirstName,
-    String newLastName,
-  ) async {
-    try {
-      UserEntry newProfile = await getUserEntryFromEmail(emailID).then((
-        result,
-      ) {
-        if (result != null) {
-          Map<String, dynamic> resultMap = result.toFirestore();
-          resultMap['username'] = newUsername;
-          resultMap['firstName'] = newFirstName;
-          resultMap['lastName'] = newLastName;
-          return UserEntry.fromMap(resultMap);
-        } else {
-          throw Exception;
-        }
-      });
-
-      final docRef = _firestore
-          .collection('users')
-          .withConverter(
-            fromFirestore: UserEntry.fromFirestore,
-            toFirestore: (UserEntry userEntry, options) =>
-                userEntry.toFirestore(),
-          )
-          .doc(emailID);
-      await docRef.set(newProfile);
-
-      return true;
-    } catch (e) {
-      debugPrint(e.toString());
-      return false;
-    }
-  }
-
-  // Gets a user entry from Cloud Firestore based on an email
-  Future<UserEntry?> getUserEntryFromEmail(String email) async {
-    final docRef = _firestore
-        .collection('users')
-        .doc(email)
-        .withConverter(
-          fromFirestore: UserEntry.fromFirestore,
-          toFirestore: (UserEntry userEntry, _) => userEntry.toFirestore(),
-        );
-    final docSnap = await docRef.get();
-    final user = docSnap.data();
-    return user;
-  }
-
-  // Chat functions start here
   // Adds a new chat entry, returns a boolean for success
   Future<bool> addChatEntry(String messageBoard, ChatEntry chatMessage) async {
     try {
