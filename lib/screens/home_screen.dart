65f2c7109d4b0c7d0f41854945c4e03098d3118c
Nathan Larsen <nlarsen@nathan2055.com>
2025-12-12 19:24:47 -0500

    Update test call on HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,7 +51,32 @@ class _HomeScreenState extends State<HomeScreen> {
             tooltip: 'User Info Test',
             onPressed: () {
               setState(() {
-                widget.authService.getUserInfo();
+                print('Old info:');
+                widget.authService.getUserInfoOld();
+                print('');
+
+                print('New info:');
+                widget.authService.getUserInfoNew();
+                print('');
+
+                print('Test system:');
+                widget.authService.validateGetters();
+                print('');
+
+                print('Old email getter:');
+                String oldEmail = widget.authService.getEmail();
+                print(oldEmail);
+                print('');
+
+                print('New email getter:');
+                String? newEmail = widget.authService.getEmailNew();
+                print(newEmail);
+                print('');
+
+                print('Test email comparison:');
+                bool emailTest = (oldEmail == newEmail);
+                print(emailTest);
+                print('');
               });
             },
           ),
