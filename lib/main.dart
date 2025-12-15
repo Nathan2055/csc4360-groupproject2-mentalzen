829ca11e255743fc1003153362dfbcd0a53c7e89
Alyvvn <muhammadalyan27@gmail.com>
2025-12-14 20:46:27 -0500

    Add FcmService instance  in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:mentalzen/firebase_options.dart';
 import 'package:mentalzen/services/authservice.dart';
 import 'package:mentalzen/screens/1-welcome_screen/welcome_screen.dart';
 import 'package:mentalzen/services/firestore_helper.dart';
+import 'package:mentalzen/services/fcm_service.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -22,18 +23,34 @@ void main() async {
   runApp(MyApp(authService, dbHelper));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp(this.authService, this.dbHelper, {super.key});
 
   final AuthService authService;
   final FirestoreHelper dbHelper;
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  late FcmService fcmService;
+
+  @override
+  void initState() {
+    super.initState();
+    fcmService = FcmService(widget.dbHelper, navigatorKey: navigatorKey);
+    fcmService.initializeMessageHandlers();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       title: 'Mental Zen',
-      home: WelcomeScreen(authService, dbHelper),
+      home: WelcomeScreen(widget.authService, widget.dbHelper, fcmService),
     );
   }
 }
